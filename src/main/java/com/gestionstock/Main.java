4762f298fad13df29861bc6a91caaeef5b23d601
Babacar NDIAYE <bndiaye@modelsis.sn>
2026-06-11 20:54:04 +0000

    contenu principale & menu lateral gauche

--- src/main/java/com/gestionstock/Main.java
+++ src/main/java/com/gestionstock/Main.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(
--- src/main/java/com/gestionstock/controller/MainController.java
+++ src/main/java/com/gestionstock/controller/MainController.java
@@ -1,4 +1,42 @@
 package com.gestionstock.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+/*
+    -@FXML: Annotation qui connecte un attribut Java à un composant déclaré dans le fichier XML via son fx:id
+    -initialize(): méthode spéciale appelée automatiquement par JavaFx après le chargement du FXML
+ */
 public class MainController {
+    @FXML
+    private StackPane contenuPrincipale;
+
+    @FXML
+    public void initialize() { afficherDashboard();}
+
+    @FXML
+    private void afficherDashboard() {
+        contenuPrincipale.getChildren().clear();
+        contenuPrincipale.getChildren().add(new Label("Dashboard"));
+    }
+
+    @FXML
+    private void afficherProduits() {
+        contenuPrincipale.getChildren().clear();
+        contenuPrincipale.getChildren().add(new Label("Produits"));
+    }
+
+    @FXML
+    private void afficherCategories() {
+        contenuPrincipale.getChildren().clear();
+            contenuPrincipale.getChildren().add(new Label("Categories"));
+    }
+
+    @FXML
+    private void afficherFournisseurs() {
+        contenuPrincipale.getChildren().clear();
+        contenuPrincipale.getChildren().add(new Label("Fournisseurs"));
+    }
+
 }
